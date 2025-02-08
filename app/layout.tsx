f6a79cb283093cb7d9a633f10aa588c8511e3e5d
vaibhavkulkarni26 <vaibhavkulkarni095@gmail.com>
2025-02-08 16:51:44 +0530

    commit-2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { ThemeProvider } from "next-themes";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,7 +15,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Vaibhav's Portfolio ",
-  description: "Vaibhav's Portfolio ",
+  description: "Morden and Minimalist JS Mastery Portfolio ",
 };
 
 export default function RootLayout({
@@ -27,7 +28,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+          <ThemeProvider
+              attribute="class"
+              defaultTheme="dark"
+              enableSystem
+              disableTransitionOnChange
+            >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
